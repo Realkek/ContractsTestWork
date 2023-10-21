085b0e7195d44693c6682bd4da5eae7dce2f547a
Mikhail Turlyakov <reelkek@gmail.com>
2023-10-21 19:37:52 +0400

    add main entities; add main packages to project

---
+++ ContractsTestWork/Models/Entities/Contract.cs
@@ -0,0 +1,9 @@
+﻿namespace ContractsTestWork.Models.Entities;
+
+public class Contract : BaseEntity
+{
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public string Customer { get; set; }
+    public List<ContractStage> Stages { get; set; }
+}
---
+++ ContractsTestWork/Models/Entities/ContractStage.cs
@@ -0,0 +1,10 @@
+﻿namespace ContractsTestWork.Models.Entities;
+
+public class ContractStage : BaseEntity
+{
+    public string Name { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public int ContractId { get; set; }
+    public Contract Contract { get; set; }
+}
